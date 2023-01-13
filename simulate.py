4ba5408b0b79dca6051bbe1b5434b6114c29e75e
Pratik <60756887+pratik-ingle@users.noreply.github.com>
2023-01-14 00:43:26 +0530

    added floor

--- simulate.py
+++ simulate.py
@@ -1,10 +1,17 @@
 
 import pybullet as p
+import pybullet_data
 import time
 
+
 physicsClint = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+planeID  = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
+
 for _ in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
