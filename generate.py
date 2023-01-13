58b945ed8c0c68b740c83e439aef3700595db811
Pratik <60756887+pratik-ingle@users.noreply.github.com>
2023-01-14 00:58:56 +0530

    added boxes

--- generate.py
+++ generate.py
@@ -1,7 +1,13 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+length, width, height = 1, 1, 1
+x ,y,z = 0, 0, 0.5
 
+length2, width2, height2 = 1, 1, 1
+x2 ,y2 ,z2 = 0, 1, 1.5
+
+pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size=[length2,width2,height2])
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -10,7 +10,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeID  = p.loadURDF("plane.urdf")
 
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for _ in range(1000):
     p.stepSimulation()
